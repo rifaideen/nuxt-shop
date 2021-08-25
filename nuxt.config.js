f4d5d9e72ea44e07eb3b2488272fdfc566cf1bbb
Rifaudeen <rifa.ahamed@askvider.com>
2021-08-25 17:43:36 +0400

    Added fontawesome assets

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,6 +18,7 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    '~/static/fontawesome/css/all.min.css',
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
