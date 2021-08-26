cc0b08bbd7aeeeaa2fa0062c2c6f075f34a994c3
Rifaudeen <rifa.ahamed@askvider.com>
2021-08-26 17:48:43 +0400

    updated footer layout

--- components/layout/Footer.vue
+++ components/layout/Footer.vue
@@ -1,37 +1,5 @@
 <template>
-  <footer>
-    <div class="container">
-      <div class="row mb-2 p-2">
-        <div class="col-lg-6">
-          Get In Touch
-          <div class="row">
-            <div class="col-12">
-              <nuxt-link to="/">Home</nuxt-link>
-            </div>
-            <div class="col-12">
-              <nuxt-link to="/about">About</nuxt-link>
-              </div>
-            <div class="col-12">
-              <nuxt-link to="/contact">Contact</nuxt-link>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-6">
-          Our Collections
-          <div class="row">
-            <div class="col-12">
-              <!-- <nuxt-link to="/films">Films</nuxt-link> -->
-            </div>
-            <div class="col-12">
-              <!-- <nuxt-link to="/genres">Genres</nuxt-link> -->
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col">&copy; {{ year }} Ghraoui Chocolates</div>
-      </div>
-    </div>
+  <footer class="mt-5">
   </footer>
 </template>
 
@@ -49,7 +17,7 @@ export default {
 <style scoped>
 footer {
   padding-top: 20px;
-  min-height: 200px;
+  min-height: 100px;
   color: grey;
 }
 </style>
