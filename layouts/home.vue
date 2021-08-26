1fccddbd28adfd1b24ccb2d647ef7478954e9f08
Rifaudeen <rifa.ahamed@askvider.com>
2021-08-26 10:50:21 +0400

    static pages and layout

---
+++ layouts/home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="dark">
+    <Header />
+    <Nuxt />
+    <Footer />
+  </div>
+</template>
+
+<style scoped>
+.dark {
+  background-color: #141414;
+  color: white;
+}
+</style>
---
+++ pages/about.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>About Us</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda quas praesentium qui cumque nam tempore incidunt ex accusamus fuga quae aperiam, nulla inventore officia asperiores nesciunt amet quisquam ea doloremque?
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
