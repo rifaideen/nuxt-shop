64a3e94456a228dd260c467390ba6ace228789c3
Rifaudeen <rifa.ahamed@askvider.com>
2021-08-26 16:46:06 +0400

    refactor to use the newly created grid components

--- pages/index.vue
+++ pages/index.vue
@@ -3,35 +3,12 @@
     <product-carousel :products="sliders" :has-actions="true" id-attribute="product_id" />
 
     <!-- Best Sellers -->
-    <div class="container mb-2">
-      <div class="row">
-        <div class="col">
-          <h2>Our Best Sellers</h2>
-        </div>
-        <div class="col text-right">
-          <nuxt-link to="/best-sellers"> See All <i class="fa fa-chevron-right"></i></nuxt-link>
-        </div>
-      </div>
-      <div class="row">
-        <div
-          class="col-4"
-          v-for="bestSeller in bestSellers"
-          :key="bestSeller.id"
-        >
-          <nuxt-link :to="`/product/${bestSeller.id}`">
-            <b-card class="best-seller" :img-src="bestSeller.image">
-              <b-card-title>
-                <div class="row">
-                  <div class="col-10">{{ bestSeller.title }}</div>
-                  <div class="col-2 text-success">
-                    <i class="fa fa-star"></i> &nbsp;
-                  </div>
-                </div>
-              </b-card-title>
-            </b-card>
-          </nuxt-link>
-        </div>
-      </div>
+    <div class="container mt-2">
+      <GridProductRow
+        title="Our Best Sellers"
+        :products="bestSellers"
+        see-all-link="/best-sellers"
+      />
     </div>
 
     <!-- Seasonal Collection -->
@@ -46,34 +23,11 @@
 
     <!-- Explore our packed collections -->
     <div class="container">
-      <div class="row">
-        <div class="col">
-          <h2>Explore Our Packed Collections</h2>
-        </div>
-        <div class="col text-right">
-          <nuxt-link to="/ready-boxes"> See All <i class="fa fa-chevron-right"></i></nuxt-link>
-        </div>
-      </div>
-      <div class="row">
-        <div
-          class="col-4"
-          v-for="box in readyBoxes"
-          :key="box.collection_id"
-        >
-          <nuxt-link :to="`/collections/${box.collection_id}`">
-            <b-card class="best-seller" :img-src="box.image">
-              <b-card-title>
-                <div class="row">
-                  <div class="col-10">{{ box.title }}</div>
-                  <div class="col-2 text-success">
-                    <i class="fa fa-star"></i> &nbsp;
-                  </div>
-                </div>
-              </b-card-title>
-            </b-card>
-          </nuxt-link>
-        </div>
-      </div>
+      <GridCollectionRow
+        title="Explore Our Packed Collections"
+        :collections="readyBoxes"
+        see-all-link="/ready-boxes"
+      />
     </div>
   </div>
 </template>
