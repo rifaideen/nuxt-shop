a13491504e62c4ad07c8ad2f9c499891cf73f22e
Rifaudeen <rifa.ahamed@askvider.com>
2021-08-30 12:04:38 +0400

    ui enhancements and added more utility methods

--- plugins/utilities.client.js
+++
@@ -1,7 +0,0 @@
-export default (context, inject) => {
-  const { router } = context.app;
-
-  inject('goBack', () => {
-    router.go(-1);
-  });
-};
---
+++ plugins/utilities.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-mixed-operators */
+export default (context, inject) => {
+  const { router } = context.app;
+
+  inject('goBack', () => {
+    router.go(-1);
+  });
+
+  function uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+      // eslint-disable-next-line no-bitwise
+      const r = Math.random() * 16 | 0;
+      // eslint-disable-next-line no-bitwise
+      const v = c === 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+  }
+
+  inject('uid', uuidv4);
+};
