0d9b2923bda2df273da4ecba94840c316f26a63d
Rifaudeen <rifa.ahamed@askvider.com>
2021-08-30 16:16:04 +0400

    shoping cart component enhancements

--- components/ShoppingCart.vue
+++ components/ShoppingCart.vue
@@ -1,33 +1,35 @@
 <template>
   <div>
-    <div class="row m-4" v-if="!cart">
-      <div class="col text-center">
-        <b>Your Ghraoui Basket is empty!</b>
-        <div class="m-4">
-          <img src="/logo.webp" alt="" />
-        </div>
-        <nuxt-link to="/best-sellers">
-          <button class="btn golden-bg">Start Order</button>
-        </nuxt-link>
-      </div>
-    </div>
-    <template v-else>
-      <div class="row mb-2 mt-4" v-for="item in items" :key="item.id">
-        <div class="col-lg-10 product-container col-sm-12 shadow-lg rounded">
-          <CartItem :item="item" @item-deleted="$fetch" />
+    <div v-if="!$fetchState.pending">
+      <div class="row m-4" v-if="items.length === 0">
+        <div class="col text-center">
+          <b>Your Ghraoui Basket is empty!</b>
+          <div class="m-4">
+            <img src="/logo.webp" alt="" />
+          </div>
+          <nuxt-link to="/best-sellers">
+            <button class="btn golden-bg">Start Order</button>
+          </nuxt-link>
         </div>
       </div>
+      <template v-else>
+        <div class="row mb-2 mt-4" v-for="item in items" :key="item.id">
+          <div class="col-lg-10 product-container col-sm-12 shadow-lg rounded">
+            <CartItem :item="item" @item-deleted="$fetch" :currency="cart.currency" />
+          </div>
+        </div>
 
-      <div class="row mb-2 mt-4">
-        <div class="col-lg-10 product-container col-sm-12 shadow-lg rounded">
-          <CartCheckout
-            :items="items"
-            :total="cart.total_amount"
-            :currency="cart.currency"
-          />
+        <div class="row mb-2 mt-4">
+          <div class="col-lg-10 product-container col-sm-12 shadow-lg rounded">
+            <CartCheckout
+              :items="items"
+              :total="cart.total_amount"
+              :currency="cart.currency"
+            />
+          </div>
         </div>
-      </div>
-    </template>
+      </template>
+    </div>
   </div>
 </template>
 
@@ -43,11 +45,6 @@ export default {
     ...mapActions('cart', ['get']),
   },
   async fetch() {
-    const { id: storeId } = this.$store.state.store;
-
-    const { data } = await this.$axios.get(`/best-sellers/${storeId}`);
-    this.bestSellers = data.data;
-
     // cart created?
     if (this.created) {
       await this.get();
