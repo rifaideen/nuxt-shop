33cc9cde5c4425fbb5065ffb26e316bdf9491693
Rifaudeen <rifa.ahamed@askvider.com>
2021-08-30 15:38:50 +0400

    cart component enhancements

--- components/ShoppingCart.vue
+++ components/ShoppingCart.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
-    <template v-if="cart">
+    <div class="row m-4" v-if="!cart">
+      <div class="col text-center">
+        <b>Your Ghraoui Basket is empty!</b>
+        <div class="m-4">
+          <img src="/logo.webp" alt="" />
+        </div>
+        <nuxt-link to="/best-sellers">
+          <button class="btn golden-bg">Start Order</button>
+        </nuxt-link>
+      </div>
+    </div>
+    <template v-else>
       <div class="row mb-2 mt-4" v-for="item in items" :key="item.id">
         <div class="col-lg-10 product-container col-sm-12 shadow-lg rounded">
           <CartItem :item="item" @item-deleted="$fetch" />
@@ -17,17 +28,6 @@
         </div>
       </div>
     </template>
-    <div class="row m-4" v-else>
-      <div class="col text-center">
-        <b>Your Ghraoui Basket is empty!</b>
-        <div class="m-4">
-          <img src="/logo.webp" alt="" />
-        </div>
-        <nuxt-link to="/best-sellers">
-          <button class="btn golden-bg">Start Order</button>
-        </nuxt-link>
-      </div>
-    </div>
   </div>
 </template>
 
