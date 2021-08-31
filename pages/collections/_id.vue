f2ac66a0d476ffcc9417bd45231008622f9e46e2
Rifaudeen <rifa.ahamed@askvider.com>
2021-08-31 15:39:59 +0400

    updated collection details page to use newly created components controlled via events

--- pages/collections/_id.vue
+++ pages/collections/_id.vue
@@ -4,42 +4,23 @@
       <i class="fa fa-chevron-left"></i>
       {{ collection.title }}
     </h1>
-    <div
-      class="row product-container shadow mb-2"
+    <ListProduct
+      :product="product"
       v-for="product in products"
       :key="product.id"
-    >
-      <div class="col-12 text-success text-right">
-        <i class="fa fa-star"></i>
-      </div>
-      <div class="col-lg-3 col-sm-4">
-        <img
-          :src="product.image"
-          :alt="product.title"
-          class="rounded-lg img-fluid"
-        />
-      </div>
-      <div class="col-lg-9 col-sm-8">
-        <nuxt-link :to="`/product/${product.id}`">
-          <b>{{ product.title }}</b>
-        </nuxt-link>
-        <p>{{ product.description }}</p>
-      </div>
-      <div class="col-12">
-        <div class="row mb-2">
-          <div class="col-lg-9 col-sm-12">
-            <b>
-              {{ product.price }} {{ product.currency }} / {{ product.unit }}
-            </b>
-          </div>
-          <div class="col-lg-3 text-center col-sm-12">
-            <button class="btn btn-primary add-to-cart-btn">
-              <i class="fa fa-plus-circle"></i> Add to Basket
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
+      @add-to-basket="onAddToBasket"
+      @increase-quantity="onIncreaseQuantity"
+      @decrease-quantity="onDecreaseQuantity"
+      :ref="`ListProduct-${product.id}`"
+    />
+    <CartControls
+      :item="selectedItem"
+      :existing-item="existingItem"
+      v-if="selectedItem !== null"
+      @item-updated="onItemUpdated"
+      @item-deleted="onItemUpdated"
+      :key="'selected-item-' + selectedItem.product_id"
+    />
   </div>
 </template>
 
@@ -48,6 +29,12 @@
 export default {
   name: 'Collection-Details',
   middleware: ['store-selection'],
+  data() {
+    return {
+      selectedItem: null,
+      existingItem: false,
+    };
+  },
   // eslint-disable-next-line consistent-return
   async asyncData({
     params, store, $axios, error,
@@ -57,7 +44,14 @@ export default {
       const collectionId = params.id;
       const collection = $axios.get(`collections/${collectionId}`);
       const products = $axios.get(`products/${storeID}/${collectionId}`);
-      const results = await Promise.all([collection, products]);
+      const items = [collection, products];
+
+      if (store.getters['cart/created']) {
+        const cart = store.dispatch('cart/get');
+        items.push(cart);
+      }
+
+      const results = await Promise.all(items);
 
       return {
         id: collectionId,
@@ -68,6 +62,50 @@ export default {
     } catch (e) {
       error(e);
     }
+  },
+  methods: {
+    /**
+     * Make it non existing item, manually construct selected item
+     * and display the modal.
+     */
+    onAddToBasket(product) {
+      this.existingItem = false;
+      const item = {
+        name: product.title,
+        product_id: product.id,
+        ...product,
+      };
+      this.selectedItem = item;
+      this.showModal();
+    },
+    // handles increase-quantity event from CartControls component.
+    onIncreaseQuantity(item) {
+      this.existingItem = true;
+      this.selectedItem = item;
+      this.showModal();
+    },
+    // handles decrease-quantity event from CartControls component.
+    onDecreaseQuantity(item) {
+      this.existingItem = true;
+      this.selectedItem = item;
+      this.showModal();
+    },
+    showModal() {
+      this.$nextTick(function () {
+        this.$bvModal.show('cart-controls');
+      });
+    },
+    // handles item-updated and item-deleted event from CartControls component.
+    async onItemUpdated(productId) {
+      this.selectedItem = null;
+      this.$bvModal.hide('cart-controls');
+      // get updated cart details
+      await this.$store.dispatch('cart/get');
+
+      // We have added reference to each ListProduct with their product id.
+      // call the component by the reference and ask it to refresh it's data.
+      this.$refs[`ListProduct-${productId}`][0].refresh();
+    },
   },
 };
 </script>
