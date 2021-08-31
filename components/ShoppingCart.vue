996341cc7603ac81313791d23e717e2227913c2b
Rifaudeen <rifa.ahamed@askvider.com>
2021-08-31 17:27:02 +0400

    updated shopping cart component

--- components/ShoppingCart.vue
+++ components/ShoppingCart.vue
@@ -1,41 +1,39 @@
 <template>
   <div>
-    <div v-if="!$fetchState.pending">
-      <div class="row m-4" v-if="items.length === 0">
-        <div class="col text-center">
-          <b>Your Ghraoui Basket is empty!</b>
-          <div class="m-4">
-            <img src="/logo.webp" alt="" />
-          </div>
-          <nuxt-link to="/best-sellers">
-            <button class="btn golden-bg">Start Order</button>
-          </nuxt-link>
+    <div class="row m-4" v-if="items.length === 0">
+      <div class="col text-center">
+        <b>Your Basket is empty!</b>
+        <div class="m-4">
+          <img src="/logo.webp" alt="" />
+        </div>
+        <nuxt-link to="/best-sellers">
+          <button class="btn golden-bg">Start Order</button>
+        </nuxt-link>
+      </div>
+    </div>
+    <template v-else>
+      <div class="row mb-2 mt-4" v-for="item in items" :key="item.id">
+        <div class="col-lg-10 product-container col-sm-12 shadow-lg rounded">
+          <CartItem
+            :item="item"
+            :currency="cart.currency"
+            @item-updated="refreshCart"
+            @item-deleted="refreshCart"
+            @item-saved-for-later="refreshCart"
+          />
         </div>
       </div>
-      <template v-else>
-        <div class="row mb-2 mt-4" v-for="item in items" :key="item.id">
-          <div class="col-lg-10 product-container col-sm-12 shadow-lg rounded">
-            <CartItem
-              :item="item"
-              :currency="cart.currency"
-              @item-updated="$fetch"
-              @item-deleted="$fetch"
-              @item-saved-for-later="$fetch"
-            />
-          </div>
-        </div>
 
-        <div class="row mb-2 mt-4">
-          <div class="col-lg-10 product-container col-sm-12 shadow-lg rounded">
-            <CartCheckout
-              :items="items"
-              :total="cart.total_amount"
-              :currency="cart.currency"
-            />
-          </div>
+      <div class="row mb-2 mt-4">
+        <div class="col-lg-10 product-container col-sm-12 shadow-lg rounded">
+          <CartCheckout
+            :items="items"
+            :total="cart.total_amount"
+            :currency="cart.currency"
+          />
         </div>
-      </template>
-    </div>
+      </div>
+    </template>
   </div>
 </template>
 
@@ -49,14 +47,14 @@ export default {
   },
   methods: {
     ...mapActions('cart', ['get', 'create']),
-  },
-  async fetch() {
-    // cart created?
-    if (!this.created) {
-      await this.create();
-    }
+    async refreshCart() {
+      // cart created?
+      if (!this.created) {
+        await this.create();
+      }
 
-    await this.get();
+      await this.get();
+    },
   },
 };
 </script>
