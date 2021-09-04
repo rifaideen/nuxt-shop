288d291a9dcb11e70ef204a0acc51362e12c5ff9
Rifaudeen <rifa.ahamed@askvider.com>
2021-09-04 15:03:25 +0400

    create gift recipient

---
+++ pages/gift-recipients/new.vue
@@ -0,0 +1,30 @@
+<template>
+<div>
+  <gift-recipient-form :countries="countries" />
+</div>
+</template>
+
+<script>
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { mapState } from 'vuex';
+
+export default {
+  layout: 'navigationOnly',
+  computed: {
+    ...mapState(['countries']),
+  },
+  async asyncData({ store }) {
+    store.commit('setNavigationTitle', 'Add a new location');
+
+    if (store.state.countries.length === 0) {
+      await store.dispatch('getCountries');
+    }
+  },
+};
+</script>
+
+<style scoped>
+.invalid-feedback {
+  display: block;
+}
+</style>
