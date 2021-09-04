e382a0707d42bb72163a33aa8fcea3122d552a05
Rifaudeen <rifa.ahamed@askvider.com>
2021-09-04 13:30:48 +0400

    Added delivery location edit page

---
+++ pages/delivery-locations/edit/_id.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <delivery-location-form :is-new="false" :location="location" :countries="countries" />
+  </div>
+</template>
+
+<script>
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { mapState } from 'vuex';
+
+export default {
+  computed: {
+    ...mapState(['countries']),
+  },
+  async asyncData({ params, $axios, store }) {
+    const { data } = await $axios.get(`/delivery-location/${params.id}?expand=country`);
+
+    if (store.state.countries.length === 0) {
+      await store.dispatch('getCountries');
+    }
+
+    return {
+      location: data,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
