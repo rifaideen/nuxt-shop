aca0f6a2cc3096849816f3a762d36659c365cbef
Rifaudeen <rifa.ahamed@askvider.com>
2021-09-04 20:42:35 +0400

    Order Placed Screen Added

---
+++ pages/order-placed.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="row text-center mt-4">
+    <div class="col">
+      <h3 class="mb-2">Your Order Is Placed</h3>
+      <p>You'll now receive confirmation on your email and mobile number.</p>
+      <p>
+        <i class="fa fa-fan fa-5x orange-color"></i>
+      </p>
+      <button class="btn golden-bg" @click="$router.push('/')">
+        Back To Home
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'navigationOnly',
+  middleware: ['authenticated'],
+  asyncData({ store }) {
+    store.commit('setNavigationTitle', 'Order Placed');
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
