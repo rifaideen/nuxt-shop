288220a8455e69a6e45459241607370465b11600
Rifaudeen <rifa.ahamed@askvider.com>
2021-09-04 16:06:01 +0400

    display more details on checkout page

--- pages/checkout.vue
+++ pages/checkout.vue
@@ -9,7 +9,7 @@
           </template>
           <template v-if="isGiftCheckout && giftRecipient">
             <b class="orange-color">Recipient Details:</b>
-            <CheckoutDeliveryComponent :location="giftRecipient" />
+            <CheckoutDeliveryComponent :is-gift-recipient="true" :location="giftRecipient" />
           </template>
         </div>
         <div class="col-12 mb-4">
@@ -69,63 +69,11 @@
       </div>
     </div>
 
-    <div class="rounded light-bg-container order-summary">
-      <div class="p-4">
-        <div class="row mb-2">
-          <div class="col-6">
-            <h4 class="orange-color">Order Summary:</h4>
-          </div>
-          <div class="col-6 text-right">
-            <sub><i>Price Inclusive of taxes</i></sub>
-          </div>
-        </div>
-        <!-- Products List -->
-        <div class="row mb-2" v-for="item in items" :key="item.id">
-          <div class="col-6">{{ item.name }}</div>
-          <div class="col-6 text-right">
-            {{ item.price }} {{ cart.currency }}
-          </div>
-        </div>
-        <hr />
+    <!-- Order Summary -->
+    <OrderSummary />
 
-        <!-- Pricing Summary -->
-        <div class="row text-info">
-          <div class="col-6">
-            <b>Subtotal:</b>
-          </div>
-          <div class="col-6 text-right">
-            <b>{{ cart.gross_total }} {{ cart.currency }}</b>
-          </div>
-        </div>
-        <div class="row mt-2">
-          <div class="col-6">VAT {{ cart.tax_percentage }}%</div>
-          <div class="col-6 text-right">
-            {{ cart.tax_amount }} {{ cart.currency }}
-          </div>
-        </div>
-        <div class="row mt-2">
-          <div class="col-6">Delivery Charge</div>
-          <div class="col-6 text-right">
-            {{ cart.delivery_charge }} {{ cart.currency }}
-          </div>
-        </div>
-        <hr />
-
-        <!-- Charge and delivery location  -->
-        <div class="row">
-          <div class="col-6">
-            <b>Total:</b>
-          </div>
-          <div class="col-6 text-right">
-            <b class="orange-color">{{ cart.total_amount }} {{ cart.currency }}</b>
-          </div>
-        </div>
-
-        <div class="row mt-2">
-          <div class="col">Delivery To: #402, ABC XYZ</div>
-        </div>
-      </div>
-    </div>
+    <!-- Review Order -->
+    <ReviewOrder />
 
     <div class="fixed-bottom">
       <div class="row">
@@ -154,6 +102,7 @@ export default {
   async asyncData({ store }) {
     store.commit('setNavigationTitle', 'Checkout');
     await store.dispatch('cart/get');
+    await store.dispatch('auth/getUser');
 
     return {
       cart: store.getters['cart/cart'],
