2694911d8167cd55d91987a02ec34c2371d2b95f
Rifaudeen <rifa.ahamed@askvider.com>
2021-09-04 15:54:04 +0400

    dl component enhancements

--- components/checkout/DeliveryComponent.vue
+++ components/checkout/DeliveryComponent.vue
@@ -1,18 +1,29 @@
 <template>
   <div class="row delivery-location mt-2">
-    <div class="col-12 mb-2"><b>Title:</b> {{ location.name }}</div>
+    <div class="col-12 mb-2">
+      <b>Title:</b>
+      {{ isGiftRecipient ? location.full_name : location.name }}
+    </div>
     <div class="col-12 mb-2">
       <b>Country:</b> {{ location.country.name }}
     </div>
     <div class="col-12 mb-2"><b>City:</b> {{ location.city }}</div>
-    <div class="col-12 mb-2"><b>Mobile:</b> {{ location.name }}</div>
+    <div class="col-12 mb-2">
+      <b>Mobile:</b>
+      {{ isGiftRecipient ? location.mobile_number : user.mobile }}
+    </div>
     <div class="col-12"><b>Address:</b> {{ location.address }}</div>
   </div>
 </template>
 
 <script>
 export default {
-  props: ['location'],
+  props: ['location', 'isGiftRecipient'],
+  computed: {
+    user() {
+      return this.$store.getters['auth/user'];
+    },
+  },
 };
 </script>
 
